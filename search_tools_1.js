79a0d8354dfebe21e89ecab74c5eefe47b7df57d
ttobhagi <126307089+ttobhagi@users.noreply.github.com>
2023-02-26 21:23:14 +0900

    Create search_tools_1.js

---
+++ search_tools_1.js
@@ -0,0 +1,25 @@
+function findToolsForBolt(boltType) {
+  let toolList = [];
+
+  switch (boltType) {
+    case "Phillips":
+      toolList.push({ name: "Phillips Screwdriver", sku: "12345", catalog: "https://example.com/catalog/12345" });
+      break;
+    case "Slotted":
+      toolList.push({ name: "Slotted Screwdriver", sku: "67890", catalog: "https://example.com/catalog/67890" });
+      break;
+    case "Hex":
+      toolList.push({ name: "Hex Key", sku: "24680", catalog: "https://example.com/catalog/24680" });
+      break;
+    default:
+      console.log("Cannot find tools for this type of bolt.");
+      break;
+  }
+
+  return toolList;
+}
+
+// Example usage
+let boltType = "Phillips";
+let tools = findToolsForBolt(boltType);
+console.log(tools);
